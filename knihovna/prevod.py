2a6ac0b27da11b3c04d738f146584251bd518203
Nikolas Erlebach <76956106+Blikyy@users.noreply.github.com>
2022-04-19 13:17:05 +0200

    Add files via upload

---
+++ knihovna/prevod.py
@@ -0,0 +1,16 @@
+class LyKm:
+    def __init__(self):
+        pass
+
+    def Ly_Km(self, num):
+        return num * 9460730472580
+
+        """ spočítá kolik světelných let je poček Km který zadáme
+            pomocí .Ly_Km(počet světelných let)  """
+
+    
+    def Km_Ly(self, num):
+        return num / 9460730472580
+
+        """spočítá kolik Km je počet světelných let který zadáme
+            pomocí .Km_Ly(počet Km)"""
---
+++ knihovna/test.py
@@ -0,0 +1,7 @@
+from prevod import LyKm
+
+objekt = LyKm()
+ly = objekt.Ly_Km(1)
+km = objekt.Km_Ly(9460730472580)
+print(ly)
+print(km)
